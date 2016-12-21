24fa0bcb560eec542a1b9f4e1ab875bf537e451e
NachoGoro <nachogoro.dev@gmail.com>
2016-12-21 20:55:04 +0000

    Improved primality test

--- primes.py
+++ primes.py
@@ -4,11 +4,47 @@ from itertools import islice
 from itertools import count
 from math import sqrt
 
-# Checks whether n is prime or not
+# Checks whether n is prime or not (using Rabin-Miller)
 def is_prime(n):
-    if n == 2 or n == 3: return True
-    if n < 2 or n % 2 == 0: return False
-    return all(n%i for i in islice(count(3,2), int(sqrt(n)-1)))
+    if n <= 1: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
+    if n < 9: return True
+    if n % 3 == 0: return False
+    if n % 5 == 0: return False
+
+    ar = [2, 3]
+    for i in ar:
+        if _witness(i, n): return False
+    return True
+
+def _witness(a, n):
+    t = 0
+    u = n-1
+    while u&1 == 0:
+        t += 1
+        u = u >> 1
+
+    xi1 = _modular_exp(a, u, n)
+    xi2 = 0
+
+    for i in range(0, t):
+        xi2 = xi1 * xi1 % n
+        if (xi2 == 1) and (xi1 != 1) and (xi1 != (n-1)): return True
+        xi1 = xi2
+    if xi1 != 1: return True
+    return False
+
+def _modular_exp(a, b, n):
+    d = 1
+    k = 0
+    while b>>k > 0:
+        k += 1
+
+    for i in range(k-1, -1, -1):
+        d = d*d%n
+        if ((b>>i) & 1) > 0: d = d*a%n
+    return d
 
 
 # Find all prime numbers up to n using the sieve of Eratosthenes
